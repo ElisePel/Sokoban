c7fe22c0242ad9f68ad0e1caeefb60b077f7cd4c
Claire-Mezieres <claire.mezieres@orange.fr>
2022-11-07 09:50:38 +0100

    ajout perso et plateau

---
+++ src/code/Personnage.java
@@ -0,0 +1,83 @@
+package code;
+/**
+ * permet d'instancier un personnage
+ * @author Claire MÃ©ziÃ¨res et Elise Pellegry
+ * @version 1.0 08/03/2022
+ */
+public class Personnage {
+	/**
+	 * attributs
+	 * i : correspond Ã  l'abscisse
+	 * j : correspond Ã  l'ordonnÃ©e
+	 */
+	private int abcissePersonnage;
+    private int ordonneePersonnage;
+    private int hauteurPersonnage;
+    private int largeurPersonnage;
+
+	/**
+	 * Constructeur de la classe Personnage
+	 * @param aP abscisse du personnage
+	 * @param oP ordonnÃ©e du personnage
+	 * @param h hauteur du personnage
+	 * @param l largeur du personnage
+	 */
+	public Personnage(int aP, int oP,int h, int l)
+	{
+    abcissePersonnage=aP;
+    ordonneePersonnage=oP;
+    hauteurPersonnage=h;
+    largeurPersonnage=l;
+	}
+
+
+	
+	/**
+	 * @return l'abscisse 
+	 */
+	public int getAbcisse() {
+		return abcissePersonnage;
+	}
+
+
+	/**
+	 * @return l'ordonne 
+	 */
+	public int getOrdonnee() {
+		return ordonneePersonnage;
+	}
+
+	/**
+	 * @return la hauteur 
+	 */
+	public int getHauteur() {
+		return hauteurPersonnage;
+	}
+
+	/**
+	 * @return la largeur 
+	 */
+	public int getLargeur() {
+		return largeurPersonnage;
+	}
+
+	/**
+	 * permet de se dÃ©placer
+	 * x : correspond Ã  l'abssice
+	 * y : correspond Ã  l'ordonnee
+	 */
+	public void deplacer(int x, int y) {
+		abcissePersonnage+=x;
+    	ordonneePersonnage+=y;
+	}
+
+	
+	/**
+	 * met Ã  jour les coordonnÃ©es
+	 * x : correspond Ã  l'abssice
+	 * y : correspond Ã  l'ordonnee
+	 */
+	public void setCoordonneesPerso(int x, int y) {
+    	abcissePersonnage=x;
+    	ordonneePersonnage=y;
+	}
--- src/code/Plateau.java
+++ src/code/Plateau.java
@@ -1,5 +1,111 @@
 package code;
 
-public class Plateau {
 
+/**
+ * permet d'instancier un plateau
+ * @author Claire MÃ©ziÃ¨res et Elise Pellegry
+ * @version 1.0 08/03/2022
+ *
+ */
+public class Plateau {
+	/**
+	 * attributs :
+	 * largeur : entier contenant la largeur de la grille
+	 * hauteur : entier contenant la hauteur de la grille
+	 * cases : HashMap qui a pour valeur case et pour clÃ© coordonnÃ©es
+	 */
+	private int largeur = 6;
+	private int hauteur = 7; 	
+	
+	/**
+	 * Constructeur par dÃ©fault de la classe Plateau
+	 */
+	public Plateau() {
+		cases = new HashMap<Coordonnee, Case>();	
+		for(int i=0;i<hauteur;i++) {
+			for(int j=0;j<largeur;j++) {
+				Coordonnee cor = new Coordonnee(i,j);
+				Case c = new CaseVide(cor);
+				cases.put(cor, c);
+			}
+		}
+	}
+	
+	/**
+	 * Constructeur
+	 * @param l : entier qui correspond Ãƒ  la largeur de la grille
+	 * @param h : entier qui correspond Ãƒ  la hauteur de la grille
+	 */
+	public Grille(int l, int h) {
+		largeur = l;
+		hauteur = h;
+		cases = new HashMap<Coordonnee, Case>();
+		
+		for(int i=0;i<hauteur;i++) {
+			for(int j=0;j<largeur;j++) {
+				Coordonnee cor = new Coordonnee(i,j);
+				Case c = new CaseVide(cor);
+				cases.put(cor, c);
+			}
+		}
+	}
+	
+	/**
+	 * @return String : affiche la grille
+	 */
+	public String afficher() {
+		String s="     ";
+		for(int i=0 ; i<largeur ; i++) {
+			s += "--" + i + "-- ";
+		}
+		s += "\n";
+		for(int i=0 ; i<hauteur ; i++) {
+			s += "--" + i + "-- ";
+			for(int j=0 ; j<largeur ; j++) {
+				Case c = getCase(i,j);
+				s += c.afficher();
+			}
+			s += "\n";
+		}
+		return s;
+	}
+
+	/**
+	 * @param i : abscisse
+	 * @param j : ordonnÃ©e
+	 * @param c : case
+	 * met Ã  jour le type de cases
+	 */
+	public void setCase(int i, int j, Case c) {
+		Coordonnee cor = new Coordonnee(i,j);
+		cases.put(cor, c);
+	}
+	
+	/**
+	 * @param i : abscisse
+	 * @param j : ordonnÃ©e
+	 * @return case
+	 */
+	public Case getCase(int i, int j) {
+		Coordonnee cor = new Coordonnee(i,j);
+		return cases.get(cor);
+	}
+	
+	/**
+	 * @return boolean : true si la grille est pleine, false sinon
+	 */
+	public boolean pleine() {
+		int n=0;
+		while(n<largeur) {
+			for(int j=0 ; j<largeur ; j++) {
+				Case c = getCase(0,j);
+				if(c instanceof CaseVide) {
+					return false;
+				}
+			}
+			n++;
+		}
+		return true;
+	}
+	
 }
